f15f4c686bda0e9be846c9541e5bc3ad0e1f5c5e
Rennan Neves de Oliveira <rennan_neves@live.com>
2020-08-11 17:43:13 -0300

    environment ok

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+/**
+ * TODO start the application here
+ */
+import path from 'path';
+
+console.log(path.dirname);
+console.log('Hello World 2');
+
+const arr = ['Foo', 'Barr'];
+
+arr.map(item => console.log(item));
